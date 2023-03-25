4260bbd25546f0f88f11c3917fdd708c521f2025
Sms-Rk <smsrk741@gmail.com>
2023-03-25 15:15:58 +0330

    new code for gpu exporter

---
+++ gpu-sm-exporter.py
@@ -0,0 +1,59 @@
+from prometheus_client.core import GaugeMetricFamily, REGISTRY, CounterMetricFamily
+from prometheus_client import start_http_server, Gauge, Enum
+import time
+from nvitop import Device, GpuProcess, NA, colored
+import os
+
+
+class GpuSmCollector(object):
+    def __init__(self, sm_target_port=9001, polling_interval_seconds=50):
+        self.sm_target_port = sm_target_port
+        self.polling_interval_seconds = polling_interval_seconds
+
+        # Prometheus metrics to collect
+
+        self.sm = GaugeMetricFamily('my_gauge_sm', 'Help text', labels=['pid'])
+
+
+    def run_metrics_loop(self):
+        """Metrics fetching loop"""
+
+        while True:
+            self.collect()
+#            print("testttttt")
+            time.sleep(self.polling_interval_seconds)
+
+    def collect(self):
+#            gauge = GaugeMetricFamily('my_gauge_sm', 'Help text', labels=['pid'])
+            devices = Device.all()
+            for device in devices:
+                processes = device.processes()
+                if len(processes) > 0:
+                    processes = GpuProcess.take_snapshots(processes.values(), failsafe=True)
+                    #processes.sort(key=lambda process: (process.username, process.pid))
+                    for snapshot in processes:
+                        pid = str(snapshot.pid)
+                        self.sm.add_metric([pid], snapshot.gpu_sm_utilization)
+#                        print(snapshot.command)
+                else:
+                    print(colored('  - No Running Processes', attrs=('bold',)))
+            return [self.sm]
+
+
+def main():
+    """Main entry point"""
+
+    polling_interval_seconds = int(os.getenv("POLLING_INTERVAL_SECONDS", "50"))
+#    app_port = int(os.getenv("APP_PORT", "80"))
+    exporter_port = int(os.getenv("EXPORTER_PORT", "9001"))
+
+    sm_metrics = GpuSmCollector(
+        sm_target_port=exporter_port,
+        polling_interval_seconds=polling_interval_seconds
+    )
+    REGISTRY.register(sm_metrics)
+    start_http_server(exporter_port)
+    sm_metrics.run_metrics_loop()
+
+if __name__ == "__main__":
+    main()
